4b86a00ece4cb72d3e20da675f3888c8941dd011
Ouroline <nanomachin3@gmail.com>
2017-08-21 22:58:38 +0700

    Initial commit

---
+++ src/discount/Visit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package discount;
+
+/**
+ *
+ * @author Mike Daniel
+ */
+public class Visit {
+    private Customer customer;
+    private Date date;
+    private double service;
+    private double product;
+}
